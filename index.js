ae309d1d26c9cee2056657dbdd4fa08e248a8653
Hsu Ping Feng <fillano.feng@gmail.com>
2019-01-03 13:59:25 +0800

    more fixes. add fit-template support for my own project.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PluginError = require('plugin-error')
 const Transform = require('stream').Transform;
-const UglifyJS = require("uglify-js");
+const UglifyJS = require("uglify-es");
 const uglifycss = require('uglifycss');
 const imagemin = require('imagemin');
 const minify = require('html-minifier').minify;
@@ -8,14 +8,17 @@ const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminSvgo = require('imagemin-svgo');
+const fit = require('fit-template');
+const log = require('fancy-log');
 
-const PLUGIN_NAME = 'multimin';
+const PLUGIN_NAME = 'gulp-multimin';
 
 const includes = {
     javascript: ['.js'],
     html: ['.html', '.htm'],
     css: ['.css'],
-    image: ['.jpeg', '.jpg', '.png', '.gif', '.svg']
+    image: ['.jpeg', '.jpg', '.png', '.gif', '.svg'],
+    fit: ['.fit']
 };
 
 const excludes = {
@@ -25,17 +28,16 @@ const excludes = {
 
 const converters = {
     javascript: function(input, options) {
-        //console.log('javascript');
         let option = options || {};
-        let data = input.toString('utf8');
+        let data = input.contents.toString('utf8');
         return new Promise((resolve, reject) => {
             try {
                 let result = UglifyJS.minify(data, option);
                 if(!!result.error) {
-                    resolve(input);
+                    reject(result.error);
                 } else {
-                    let buf = Buffer.from(result.code, 'utf8')
-                    resolve(buf);
+                    input.contents = Buffer.from(result.code, 'utf8')
+                    resolve(input);
                 }
             } catch(e) {
                 reject(e.message);
@@ -43,37 +45,36 @@ const converters = {
         });
     },
     html: function(input, options) {
-        //console.log('html');
         let option = {};
         Object.keys(options).forEach(k => {
             option[k] = options[k];
         });
-        let data = input.toString('utf8');
+        let data = input.contents.toString();
         return new Promise((resolve, reject) => {
             try {
-                resolve(Buffer.from(minify(data, option), 'utf8'));
+                input.contents = Buffer.from(minify(data, option), 'utf8');
+                resolve(input);
             } catch(e) {
                 reject(e.message);
             }
         });
     },
     css: function(input, options) {
-        //console.log('css');
         let option = {};
         Object.keys(options).forEach(k => {
             option[k] = options[k];
         });
-        let data = input.toString('utf8');
+        let data = input.contents.toString('utf8');
         return new Promise((resolve, reject) => {
             try {
-                resolve(Buffer.from(uglifycss.processString(data, option), 'utf8'));
+                input.contents = Buffer.from(uglifycss.processString(data, option), 'utf8');
+                resolve(input);
             } catch(e) {
                 reject(e.message);
             }
         });
     },
     image: function(input, options) {
-        //console.log('image');
         let option = {
             plugins: [
                 imageminJpegtran(),
@@ -89,11 +90,63 @@ const converters = {
         Object.keys(options).forEach(k => {
             option[k] = options[k];
         });
-        try {
-            return imagemin.buffer(input, option);
-        } catch(e) {
-            reject(e.message);
-        }
+        return new Promise((resolve, reject) => {
+            try {
+                imagemin.buffer(input.contents, option)
+                .then(data => {
+                    input.contents = data;
+                    resolve(input);
+                }, reason => {
+                    reject(reason);
+                });
+            } catch(e) {
+                reject(e.message);
+            }
+        })
+    },
+    fit: function(input, options) {
+        return new Promise((resolve, reject) => {
+            try {
+                if(options.toString().indexOf('[object Map]') === 0) {
+                    for(let k of options.keys()) {
+                        if(input.path.match(k) || input.path.indexOf(k) > -1) {
+                            input.contents = Buffer.from(fit(input.contents.toString('utf8'))(options.get(k)), 'utf8');
+                            input.path = input.path.substr(0, input.path.length - 4);
+                        }
+                    }
+                } else {
+                    Object.keys(options).forEach(k => {
+                        if(input.path.indexOf(k) > -1) {
+                            input.contents = Buffer.from(fit(input.contents.toString('utf8'))(options[k]), 'utf8');
+                            input.path = input.path.substr(0, input.path.length - 4);
+                        }
+                    });
+                }
+                //resolve(input);
+                if(input.path.indexOf('.js') === input.path.length - 3) {
+                    converters.javascript(input)
+                    .then(result => {
+                        resolve(result);
+                    }, reason => {
+                        reject(reason);
+                    });
+                }
+                if(input.path.indexOf('.html') === input.path.length - 5 || input.path.indexOf('.htm') === input.path.length - 4) {
+                    converters.html(input, {
+                        collapseWhitespace: true,
+                        minifyCSS: true,
+                        minifyJS: true,
+                        removeComments: true,
+                    }).then (result => {
+                        resolve(result);
+                    }, reason => {
+                        reject(reason);
+                    });
+                }
+            } catch(e) {
+                reject(e.message);
+            }
+        });
     }
 };
 
@@ -112,7 +165,7 @@ module.exports = function(options, addons) {
             let type = '';
             Object.keys(includes).forEach(i => {
                 includes[i].forEach(l => {
-                    if(file.path.indexOf(l) > -1) {
+                    if(file.path.indexOf(l) === (file.path.length - l.length)) {
                         included = true;
                         type = i;
                         Object.keys(excludes).forEach(m => {
@@ -135,10 +188,11 @@ module.exports = function(options, addons) {
                 }
                 let opt = {};
                 if(!!options && !!options[type]) opt = options[type];
-                converters[type](file.contents, opt)
+                log(`[${type}] ${file.path}`);
+                converters[type](file, opt)
                 .then(result => {
-                    file.contents = result;
-                    cb(null, file);
+                    //file.contents = result;
+                    cb(null, result);
                 }, error => {
                     console.log(file.path);
                     this.emit('error', new PluginError(PLUGIN_NAME, error));
